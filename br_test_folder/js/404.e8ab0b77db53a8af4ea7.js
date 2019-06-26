4c67b26ae626364977f1b7b46e337e45791e5c3f
Mahdi <m.pourziaei@gmail.com>
2019-06-26 10:33:32 +0800

    Updates

--- br_test_folder/js/404.e8ab0b77db53a8af4ea7.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([["404"],{689:function(e,r,t){"use strict";t.r(r);var a=t(0),n=t.n(a),o=t(35),c=t(3),s=t(124),d=function(){return n.a.createElement(s.a,{header:Object(c.a)("Oops, page not available."),error_code:404,messages:[Object(c.a)("The page you requested could not be found. Either it no longer exists or the address is wrong. Please check for any typos.")],redirect_url:o.c.trade,redirect_label:Object(c.a)("Return to Trade")})};t.d(r,"default",function(){return d})}}]);
-//# sourceMappingURL=404.e8ab0b77db53a8af4ea7.js.map
